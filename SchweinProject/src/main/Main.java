170aa58306313d000311b6b87158f476af32ad82
Jo Wagner <jowagner@limago.de>
2023-07-14 10:53:29 +0200

    initial commit

---
+++ SchweinProject/src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import de.tiere.Schwein;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+
+        System.out.println(Schwein.getCounter());
+
+        Schwein piggy = new Schwein("Piggy");
+
+        System.out.println(Schwein.getCounter());
+
+        Schwein babe = new Schwein("Babe");
+
+        System.out.println(Schwein.getCounter());
+
+        piggy = null;
+        babe = null;
+
+        System.gc();
+
+        Thread.sleep(1000);
+
+        System.out.println(Schwein.getCounter());
+
+        System.out.println("Hello world!");
+    }
+}
